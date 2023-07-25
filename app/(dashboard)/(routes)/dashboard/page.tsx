2a985982b6fdfc16656b8194c9ef0f244fffa973
Calvin Costa <costacalvin0@gmail.com>
2023-07-25 00:30:05 -0400

    set up next.js project + routes

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+const DashboardPage = () => {
+  return <Button variant="destructive">Bomb</Button>;
+};
+
+export default DashboardPage;
